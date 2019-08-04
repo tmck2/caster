d3891124793e996b2630d7ce752a62335c7405ed
tmck2 <tony.w.mckinney@gmail.com>
2019-08-04 12:52:43 -0400

    intersections working (mostly) have to fix coord system

--- src/App.fs
+++ src/App.fs
@@ -11,6 +11,10 @@ type Vec2 =
     static member (+) (a:Vec2, b:Vec2):Vec2 = { x = a.x + b.x; y = a.y + b.y }
     static member (-) (a:Vec2, b:Vec2):Vec2 = { x = a.x - b.x; y = a.y - b.y }
     static member mag (v:Vec2) = sqrt(v.x*v.x+v.y*v.y)
+    static member dot (v1:Vec2) (v2:Vec2) = v1.x*v2.x+v1.y*v2.y
+    static member normalize (v:Vec2) =
+        let d = Vec2.mag v
+        1.0/d * v
     static member rotate (rads:float) (v:Vec2) =
         { v with 
             x = v.x * cos(rads) + v.y * sin(rads)
@@ -84,8 +88,8 @@ let mapData = [
 let initState = {
     Ticks = 0.
     Player = {
-        Position = { x = 13.; y = 11. }
-        Direction = { x = 0.; y = -1. }
+        Position = { x = 64.; y = 64. }
+        Direction = { x = sqrt(2.); y = -sqrt(2.) }
     }
     CameraPlane = { x = 0.66; y = 0. }
     Level = Level.loadLevel mapData
@@ -128,21 +132,32 @@ type Graphics2d(ctx:CanvasRenderingContext2D) =
         ctx.lineTo (v2.x, v2.y)
         ctx.stroke()
 
+    member this.fillCircle (c:Vec2) (r:float) (color:string) =
+        ctx.beginPath()
+        ctx.fillStyle <- !^color
+        ctx.arc(c.x, c.y, r, 0., 2. * Math.PI)
+        ctx.fill()
+
 module OverviewRenderer =
 
     let (CellSize:float) = 16.
-    let (PlayerSize:float) = CellSize / 1.25
+    let (PlayerSize:float) = CellSize / 1.5
+
+    let cellToWorld offset x y = (CellSize * {x=float(x); y=float(y)}) + offset
 
     let renderGrid (gfx:Graphics2d) offset level =
+        for y in [0..level.Height-1] do
+            for x in [0..level.Width-1] do
+                gfx.strokeRect (cellToWorld offset x y) {x=CellSize; y=CellSize} "rgb(200,200,200)"
+
+    let renderLevel (gfx:Graphics2d) offset level =
         for y in [0..level.Height-1] do
             for x in [0..level.Width-1] do
                 let fillStyle =
                     match level.Map.[y].[x] with
                     | Solid _ -> "#ffffff"
                     | _ -> "#263545"
-                let v = (CellSize * ({x=float(x); y=float(y)})) + offset
-                gfx.fillRect v {x=CellSize; y=CellSize} fillStyle
-                gfx.strokeRect v {x=CellSize; y=CellSize} "rgb(200,200,200)"
+                gfx.fillRect (cellToWorld offset x y) {x=CellSize; y=CellSize} fillStyle
 
     let renderPlayer (gfx:Graphics2d) (offset:Vec2) gameState =
         let { Player=player; CameraPlane=camera } = gameState
@@ -164,6 +179,8 @@ module OverviewRenderer =
         let { Level = level } = gameState
 
         gfx.fillRect {x=0.; y=0.} {x=640.; y=400.} "#263545"
+
+        renderLevel gfx offset level
 
         renderGrid gfx offset level
 
@@ -205,10 +222,44 @@ let update t gameState =
         CameraPlane=updated_camera
         Ticks = t}
 
+let intersect p0 l0 l n:Vec2 option =
+    let denom = Vec2.dot l n
+    if denom <= 0. then
+        None
+    else
+        let d = (Vec2.dot (p0 - l0) n) / denom
+        Some ((d * l) + l0)
+
+(*
+    TODO: On intersections, y coordinates are upside down
+*)
+
+let test (gfx:Graphics2d) updatedState =
+    gfx.fillRect {x=0.;y=0.} {x=640.;y=400.} "#263545"
+
+    let {Player=player} = updatedState
+    let {Position=p;Direction=r} = player
+    let off = {x=200.;y=100.}
+    let size = {x=128.;y=128.}
+
+    let paintIntersection x =
+        match x with
+        | Some i -> gfx.fillCircle (i+off) 5. "red"
+        | None -> ()
+
+    intersect {x=0.;y=128.} p r {x=0.;y= 1.} |> paintIntersection
+    intersect {x=128.;y=0.} p r {x=1.; y=0.} |> paintIntersection
+    intersect {x=0.;y=0.} p r {x= -1.;y= 0.} |> paintIntersection
+    intersect {x=0.;y=0.} p r {x= 0.;y= -1.} |> paintIntersection
+
+    gfx.strokeRect off size "white"
+    gfx.strokeLine (p + off) (600. * r + p + off) "white"
+
 let rec gameLoop (gfx:Graphics2d) t gameState =
     let updatedState = update t gameState
 
-    OverviewRenderer.render gfx {x=0.; y=0.} updatedState
+    //OverviewRenderer.render gfx {x=0.; y=0.} updatedState
+    test gfx updatedState
 
     window.requestAnimationFrame(fun t -> (gameLoop gfx t updatedState)) |> ignore
 
